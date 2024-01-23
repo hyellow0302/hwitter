c62a256ccfa751db84c6ab77edee4b0836e45ba1
Hyellow0302 <lhi030210@gmail.com>
2024-01-23 18:22:38 +0900

    fix: 비용 절감을 위한 마운트시에만 onSnapshot함수 구독하게끔 수정

--- src/component/timeline.tsx
+++ src/component/timeline.tsx
@@ -1,6 +1,7 @@
+import { Unsubscribe } from "firebase/auth";
 import {
   collection,
-  getDocs,
+  limit,
   onSnapshot,
   orderBy,
   query,
@@ -27,31 +28,37 @@ const Wrapper = styled.div`
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createAt", "desc")
-    );
-    // 데이터베이스 및 쿼리와 실시간 연결을 생성하는 함수 onSnapshot()
-    // 해당 쿼리에 새 요소가 생성되거나, 요소가 삭제되었거나 또는 업데이트 됐을 때 쿼리에 알려주는 함수임.
-    // 전과 다른 점: 전엔 그냥 getDocs해서 가져오기만 했다면 지금은 쿼리에 리스너를 추가해서 변동되었을 때 쿼리에게 알려주어서 실시간으로 보여주게끔 함.
-    await onSnapshot(tweetsQuery, (snapshot) => {
-      const tweets = snapshot.docs.map((doc) => {
-        // console.log(doc.data());
-        const { tweet, createAt, userId, username, photo } = doc.data();
-        return {
-          tweet,
-          createAt,
-          userId,
-          username,
-          photo,
-          id: doc.id,
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createAt", "desc"),
+        limit(25)
+      );
+      // 데이터베이스 및 쿼리와 실시간 연결을 생성하는 함수 onSnapshot()
+      // 해당 쿼리에 새 요소가 생성되거나, 요소가 삭제되었거나 또는 업데이트 됐을 때 쿼리에 알려주는 함수임.
+      // 전과 다른 점: 전엔 그냥 getDocs해서 가져오기만 했다면 지금은 쿼리에 리스너를 추가해서 변동되었을 때 쿼리에게 알려주어서 실시간으로 보여주게끔 함.
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          // console.log(doc.data());
+          const { tweet, createAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweets(tweets);
+        return () => {
+          unsubscribe && unsubscribe();
         };
       });
-      setTweets(tweets);
-    });
-  };
-  useEffect(() => {
+    };
     fetchTweets();
   }, []);
   return (
