8c6123ba5d8fc50ee337b4d201a35fc8d588e488
Hyellow0302 <lhi030210@gmail.com>
2024-01-23 16:52:08 +0900

    [feat] data 쿼리 작성, data 불러오기

---
+++ src/component/timeline.tsx
@@ -0,0 +1,43 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { db } from "../firebase";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createAt: number;
+}
+
+const Wrapper = styled.div``;
+
+export default function Timeline() {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createAt", "desc")
+    );
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      // console.log(doc.data());
+      const { tweet, createAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return <Wrapper>{JSON.stringify(tweets)}</Wrapper>;
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,18 @@
 import { styled } from "styled-components";
 import PostTweetForm from "../component/post-tweet-form";
+import Timeline from "../component/timeline";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  doverflow-y: scroll;
+`;
 
 export default function Home() {
   return (
     <Wrapper>
       <PostTweetForm />
+      <Timeline />
     </Wrapper>
   );
 }
