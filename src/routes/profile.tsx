64e568f20287a2321884b921164585bee9a5a19f
Hyellow0302 <lhi030210@gmail.com>
2024-01-30 14:31:58 +0900

    feat: 프로필 변경 기능 추가

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,94 @@
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+import { styled } from "styled-components";
+import { auth, storage } from "../firebase";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1fd9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const Name = styled.span`
+  font-size: 22px;
+`;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  // 파일 선택시 저장하기 위한 함수
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    // 파일이 1개가 선택되었다면
+    if (files && files.length === 1) {
+      // 파일을 선언하고
+      const file = files[0];
+      // 파일을 저장할 곳을 찾기
+      const locationRef = ref(storage, `avatars/${user?.uid}`); // 이렇게 해야 유저가 사진을 계속 변경해도 모두 저장하지않고 덮어쓰기 할 수 있음 (비용절약)
+      // uploadBytes로 파일을 업로드
+      const result = await uploadBytes(locationRef, file);
+      // 사진 url을 얻어옴
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {Boolean(avatar) ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+    </Wrapper>
+  );
 }
